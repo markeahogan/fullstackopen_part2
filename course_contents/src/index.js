b01f01e658b14a212965bf123aacdfa373d09c0c
Mark Hogan <mark@hiddenltd.com>
2019-09-09 12:55:31 +0100

    2.4: Course contents step9

--- course_contents/src/index.js
+++ course_contents/src/index.js
@@ -6,7 +6,7 @@ const Header = props =>
 
 const Total = ({parts}) => {
   const sum = parts.reduce((sum, x) => sum += x.exercises, 0);
-  return (<p>total of {sum} exercises</p>)
+  return (<p><strong>total of {sum} exercises</strong></p>)
 }  
 
 const Part = props =>
@@ -27,31 +27,56 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
+    const courses = [
+        {
+          name: 'Half Stack application development',
+          id: 1,
+          parts: [
             {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
+              name: 'Fundamentals of React',
+              exercises: 10,
+              id: 1
             },
             {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
+              name: 'Using props to pass data',
+              exercises: 7,
+              id: 2
             },
             {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
+              name: 'State of a component',
+              exercises: 14,
+              id: 3
+            },
+            {
+              name: 'Redux',
+              exercises: 11,
+              id: 4
             }
-        ]
-    }
+          ]
+        }, 
+        {
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewares',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
+      ]
     
+    const courseElements = () => courses.map(x => <Course course={x} />);
     return (
-    <div>
-        <Course course={course} />
-    </div>
+        <div>
+            {courseElements()}
+        </div>
     )
   }
 
