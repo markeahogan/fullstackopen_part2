3756f2c2bee352acceee71c5718917f54f6b9b09
Mark Hogan <mark@hiddenltd.com>
2019-09-09 12:59:57 +0100

    2.5: separate module

--- course_contents/src/index.js
+++ course_contents/src/index.js
@@ -1,30 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = props =>
-  <h1>{props.course}</h1>
-
-const Total = ({parts}) => {
-  const sum = parts.reduce((sum, x) => sum += x.exercises, 0);
-  return (<p><strong>total of {sum} exercises</strong></p>)
-}  
-
-const Part = props =>
-  <p>{props.part.name} {props.part.exercises}</p>
-
-const Content = props => (
-  <div>
-      {props.parts.map((x)=> <Part part={x} />)}    
-  </div>
-)
-
-const Course = ({course}) => {    
-    return (<div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>)
-}
+import Course from './components/course'
 
 const App = () => {
     const courses = [
